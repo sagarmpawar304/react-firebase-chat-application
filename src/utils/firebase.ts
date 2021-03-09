192dcee0f6b4d1fda21d5d195b0d19dbb86e7721
smpawar001 <smpawar001@gmail.com>
2021-03-09 18:12:01 +0530

    firebase setup creacted

---
+++ src/utils/firebase.ts
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyAWjBh4z9bYJ3Vc4N8KsCpwfkmV2j9G3I8',
+  authDomain: 'react-firebase-app-901a7.firebaseapp.com',
+  projectId: 'react-firebase-app-901a7',
+  storageBucket: 'react-firebase-app-901a7.appspot.com',
+  messagingSenderId: '575990615155',
+  appId: '1:575990615155:web:803e7a954e708d3fab6e06',
+};
+
+const fireBaseApp = firebase.initializeApp(config);
