a6f28ad52aea85f24c3171bbcc6c80d8588faea5
smpawar001 <smpawar001@gmail.com>
2021-03-17 11:59:10 +0530

    started to build chat top; created room description modal

---
+++ src/components/Chat-Window/Top/RoomInfoBtnModal.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button } from 'antd';
+
+import { Room } from '../../../context/rooms.context';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { useModalState } from '../../../utils/customHooks';
+import Modal from 'antd/lib/modal/Modal';
+import Title from 'antd/lib/typography/Title';
+import Text from 'antd/lib/typography/Text';
+
+const RoomInfoBtnModal = () => {
+  const description = useCurrentRoom((v: Room) => v.description);
+  const name = useCurrentRoom((v: Room) => v.name);
+
+  const { isOpen, open, close } = useModalState();
+  return (
+    <>
+      <Button type="link" onClick={open}>
+        Room Info
+      </Button>
+
+      <Modal
+        visible={isOpen}
+        title={`About ${name}`}
+        footer={
+          <Button type="primary" block onClick={close}>
+            Close
+          </Button>
+        }
+        onCancel={close}
+      >
+        <Title level={5} className="mb-1">
+          Description
+        </Title>
+        <Text>{description}</Text>
+      </Modal>
+    </>
+  );
+};
+
+export default RoomInfoBtnModal;
--- src/components/Chat-Window/Top/index.tsx
+++ src/components/Chat-Window/Top/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import { useCurrentRoom } from '../../../context/current-room.context';
 import { Room } from '../../../context/rooms.context';
+import { useMediaQuery } from '../../../utils/customHooks';
+import Title from 'antd/lib/typography/Title';
+import { Link } from 'react-router-dom';
+import { LeftCircleTwoTone } from '@ant-design/icons';
+import { Button } from 'antd';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 const Top = () => {
   const name = useCurrentRoom((v: Room) => v.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery('(max-width: 768px)');
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <Title level={4}>
+          <Link to="/">
+            <LeftCircleTwoTone
+              size={16}
+              className={
+                isMobile ? 'd-inline-block p-0 mr-2 link-unstyled' : 'd-none'
+              }
+            />
+            <span className="text-disappear">{name}</span>
+          </Link>
+        </Title>
+
+        <Button type="text">Todo</Button>
+      </div>
+
+      <div className="d-flex justify-content-between align-items-center">
+        <span>Todo</span>
+        <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 };
 
 export default Top;
